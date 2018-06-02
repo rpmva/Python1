c3017100c27567892595c2951dc389e1d467f068
Ryan McGovern <rpmva@udel.edu>
2018-06-01 23:25:53 -0400

    Chapter 2 Python

---
+++ chapter2.py
@@ -0,0 +1,24 @@
+#Chapter 2
+#Floor Division: // rounds down
+#Modulo: %; Remainder
+#Negation: -
+#Absolute value: abs()
+#Exponent: **
+#Even one float type results in a float
+#Use type to determine Data type
+
+#Chapter 2 Exercise
+#Variables and Steps
+x = 10
+y = .30
+xy = x * y
+z = .05
+xyz = xy * z
+s = 7.50
+
+#Output Solution
+price = x - xy + xyz + s
+
+#Converting float to string and printing string out
+p = str(price)
+print "The price is " + "$" + p
