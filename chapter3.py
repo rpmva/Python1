3606e198f9c0a8a20c44cee470573e9f5dc2a0dc
Ryan McGovern <rpmva@udel.edu>
2018-06-02 02:01:24 -0400

    Chapter 3 Python

---
+++ chapter3.py
@@ -0,0 +1,23 @@
+#Chapter 3
+
+#Helpful Hints
+#1) Running Python in Terminal: cd python1 -> python chapter2.py
+#2) Making New directory -> mkdir ...whatever
+#3) Github commands: Git Add . , Git Commit - m "blah blah blah", Git Push
+
+#Notes
+#if...
+#else...
+#try:
+    #5/0
+#except:
+    #print please don't do that
+
+#EXERCISE
+num = 12
+if num > 0:
+    print "You have money"
+elif num == 0:
+    print "You're out"
+else:
+    print "You seem to be in debt"
