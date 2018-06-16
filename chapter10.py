6ab0a749579eecee832ad0e6975f26dd83671d28
Ryan McGovern <rpmva@udel.edu>
2018-06-16 01:03:18 -0400

    Chapter 10 Notes

---
+++ chapter10.py
@@ -0,0 +1,41 @@
+#Objects we are already using
+#1) mydict.keys() = ['peaches', 'papaya', 'apples']
+#2) mynum.hex() =''
+#3)mylist.append(999) -> mylist() = [0,1,2,3,4,5,8.999]
+#4)mystring.title() -> 'Hello, World'
+
+#Objects
+#users = ['ojohnson', 'dhellman', 'mblum']
+#users = [['ojohnson', 'Olwen','Johnson'],['dhellman','Doug','Hellman'],['mblum','Manny','Blum']]
+#username in first index, first name in second index, last name in third index
+#users[0] would get a user list, to get a username you would use users[0][0]
+
+#Vocab
+#Attributes -> values an object has
+#Methods -> functions attached to your object
+#Instance -> when you make a new object, it's called an instance
+#Class -> blueprint for an object; defines methods and attributes for your object along with setting default values
+#Subclass -> basing a new class off of an existing method, you are subclassing it
+
+#Example
+#print me
+#Username: rpmva
+#Ryan McGovern
+#Joined: 6/16/2018
+#Last seen: 6/12/2018
+#me.get_date_string(me.joined) = '6/16/2018'
+#me.first_name = "Ryan"
+#me.last_name = "McGovern"
+#me.username = "rpmva"
+
+#Pet Store
+#attributes || methods
+#title-str     change title
+#owner-str     change owner
+#location-str
+
+#animals
+#attributes || methods
+#count-int     change count
+#type-list     change list
+#price-float   chnage price  
