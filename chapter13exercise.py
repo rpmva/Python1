a7e79fb967e816a9068a7f3a3c40898f5876902b
Ryan McGovern <rpmva@udel.edu>
2018-06-18 15:38:34 -0400

    Chapter 14 Notes

---
+++ chapter13exercise.py
@@ -0,0 +1,37 @@
+#Exercise 1
+# from random import random
+# x = 0
+# while (random != 0 and random != 1):
+#     print random()
+#     print "Test item number: {}".format(x)
+#     x = x + 1
+#     if random == 0 or random == 1:
+#         print "Test complete!"
+#         break
+
+#Exercise 2
+from random import randint
+print "Welcome to the number guessing game!"
+print "I have my number..."
+
+MyNum = randint(1,10);
+MyGuess = ""
+
+def check_guess(MyGuess,MyNum):
+
+    if MyGuess > MyNum:
+        print "That's too high. Try again!"
+        return False
+    elif MyGuess < MyNum:
+        print "That's too low. Try again!"
+        return False
+    else:
+        print "You got it! Thanks for playing!"
+        return True
+
+while MyGuess != MyNum:
+    MyGuess = raw_input("What is your guess [1-10]? ")
+    if MyGuess == MyNum:
+        break
+
+    check_guess(MyGuess,MyNum)
---
+++ chapter14.py
@@ -0,0 +1,39 @@
+#Planning on how to break up a program
+#Ex:
+#Song
+#---
+#Title
+#Artist
+#Music file
+#Play time
+#Genre
+
+#Album
+#---
+#A list of songs
+#Artist(s)
+#Label
+#Year published
+
+#Functions
+#---
+#Play a song
+#Play a random song of an Album
+#Play a random album
+
+#Including Modules from the File's Directory
+#books.py -> contains class Book
+#from books import Book
+
+#Directory Structure example
+#myproject/
+    #-main.py
+    #-classes/
+        #monsters.py
+        #hero.py
+        #treasure.py
+
+#Determing current path
+#import sys
+#for directory in sys.path:
+    #print directory 
